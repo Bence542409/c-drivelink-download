00abcf3ccd7ba9d823000e5303cc741f7b1b6f06
Németh Bence <nemeth.bence3a@gmail.com>
2025-09-02 17:52:13 +0200

    Add files via upload

---
+++ drivelink/drivelink/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace drivelink
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Megosztott mappa URL: ");
+            string link = Console.ReadLine();
+            link = link.Remove(0, 39);
+            link = "https://drive.google.com/uc?export=download&id=" + link;
+            link = link.Remove(link.Length - 12, 12);
+            Console.Write("Letöltő URL: "+link);
+        }
+    }
+}
